03410f12ff91e617e65477e29ab60f78b8336d55
Jan <skv.soma@gmail.com>
2024-03-03 16:56:48 +0100

    PHP Code for Login Form

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ChatApp_PHP</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
-</head>
+<?php include_once "header.php"; ?>
 <body>
     <div class="wrapper">
         <section class="form signup">
@@ -40,7 +32,7 @@
                     <input type="submit" value="Continue to chat">
                 </div>
             </form>
-            <div class="link">Already signed up? <a href="#">Login now</a></div>
+            <div class="link">Already signed up? <a href="login.php">Login now</a></div>
         </section>
     </div>
 
