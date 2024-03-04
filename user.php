7ba4446268138ac3035ec0f346848f05a4a2908d
Jan <skv.soma@gmail.com>
2024-03-04 16:56:19 +0100

    Making Users Page Dynamic

--- user.php
+++ user.php
@@ -1,13 +1,29 @@
+<?php 
+  session_start();
+  if(!isset($_SESSION['unique_id'])){
+    header("location: login.php");
+  }
+?>
+
 <?php include_once "header.php"; ?>
+
 <body>
     <div class="wrapper">
         <section class="users">
             <header>
+                <?php 
+                    include_once "php/config.php";
+                    $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
+                    if(mysqli_num_rows($sql) > 0){
+                        $row = mysqli_fetch_assoc($sql);
+                    }
+                ?>
+
                 <div class="content">
-                    <img src="movie9.png" alt="">
+                    <img src="php/images/<?php echo $row['img'] ?>" alt="">
                     <div class="details">
-                        <span>Coding Nepal</span>
-                        <p>Active now</p>
+                        <span><?php echo $row['fname'] . " " . $row['lname'] ?></span>
+                        <p><?php echo $row['status'] ?></p>
                     </div>
                 </div>
                 <a href="#" class="logout">Logout</a>
@@ -18,7 +34,8 @@
                 <button><i class=" fas fa-search"></i></button>
             </div>
             <div class="users-list">
-                <a href="#">
+                
+            <!--<a href="#">
                     <div class="content">
                         <img src="movie9.png" alt="">
                         <div class="details">
@@ -82,7 +99,7 @@
                         </div>
                     </div>
                     <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
+                </a> -->
                 
             </div>
         </section>
